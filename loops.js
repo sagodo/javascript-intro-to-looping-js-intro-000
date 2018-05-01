00a784fe8df75e1c5d7d62514c1f6ed592da7c8b
Setor Agodo <setor.k.agodo@gmail.com>
2018-05-01 16:01:59 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+  let (i=1; i < 25; i++){
+    if (i===1){
+      array.push('I am 1 strange loop.')}
+      else {
+        array.push('I am ${1} strange loops.')}
+    }
+  }
+  return array
+}
